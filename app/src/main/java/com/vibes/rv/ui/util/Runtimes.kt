257a391169db7c4384f2078b1db72ff0079abee0
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-12 00:21:40 +0300

    chore: add composable isUserAppDark()

--- app/src/main/java/com/vibes/rv/ui/util/Runtimes.kt
+++ app/src/main/java/com/vibes/rv/ui/util/Runtimes.kt
@@ -1,9 +1,23 @@
 package com.vibes.rv.ui.util
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import com.vibes.rv.data.preference.ThemeMode
+import com.vibes.rv.data.preference.rememberUserThemeMode
 
 @Composable
 fun <T, R> rememberAndMap(value: T, mapper: (T) -> R): R {
     return remember(value) { mapper(value) }
+}
+
+@Composable
+fun isUserAppDark(): Boolean {
+    val theme by rememberUserThemeMode()
+    return when(theme) {
+        ThemeMode.SYSTEM -> isSystemInDarkTheme()
+        ThemeMode.LIGHT -> false
+        ThemeMode.DARK -> true
+    }
 }
