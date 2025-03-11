a052921ed40ff634bbd2d80a072dd0a99cba6d18
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-11 18:44:53 +0300

    chore: request read local audio

--- app/src/main/java/com/vibes/rv/MainActivity.kt
+++ app/src/main/java/com/vibes/rv/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.vibes.rv
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,9 +9,22 @@ import androidx.activity.enableEdgeToEdge
 import com.vibes.rv.ui.MainAppUi
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        val permissions get(): Array<String> {
+            val audioPermission =
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                    Manifest.permission.READ_MEDIA_AUDIO
+                else
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+
+            return arrayOf(audioPermission)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        requestPermissions(permissions, 0)
         setContent { MainAppUi() }
     }
 }
