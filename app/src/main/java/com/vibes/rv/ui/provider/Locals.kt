006c4b921be6e89196ac4b1b7fc298e91e940673
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-11 14:10:41 +0300

    chore: add local app context

---
+++ app/src/main/java/com/vibes/rv/ui/provider/Locals.kt
@@ -0,0 +1,6 @@
+package com.vibes.rv.ui.provider
+
+import androidx.compose.runtime.compositionLocalOf
+import com.vibes.rv.ui.theme.CatppuccinColor
+
+internal val LocalPalette = compositionLocalOf { CatppuccinColor.MACHIATTO }
