12e71354768e0daf3d3ae8d06e829c795283c875
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-12 21:06:32 +0300

    chore: add VibesViewModel

---
+++ app/src/main/java/com/vibes/rv/VibesViewModel.kt
@@ -0,0 +1,32 @@
+package com.vibes.rv
+
+import android.app.Application
+import android.content.ComponentName
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.MoreExecutors
+import com.vibes.rv.service.PlaybackService
+
+@Stable
+class VibesViewModel(
+    application: Application
+): AndroidViewModel(application) {
+    private var mediaController: MediaController? by mutableStateOf(null)
+
+    init {
+        val sessionToken = SessionToken(application, ComponentName(application, PlaybackService::class.java))
+        MediaController.Builder(application, sessionToken)
+            .buildAsync()
+            .apply {
+                addListener(
+                    { mediaController = get() },
+                    MoreExecutors.directExecutor()
+                )
+            }
+    }
+}
