b813dd5b3b3197b855d3081a015cd2e26002a8ad
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-12 21:09:34 +0300

    chore: add more app destinations

--- app/src/main/java/com/vibes/rv/ui/Destination.kt
+++ app/src/main/java/com/vibes/rv/ui/Destination.kt
@@ -5,14 +5,56 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed class Destination() {
     @Serializable
-    data object Home : Destination()
+    data object Tabbed : Destination() {
+        @Serializable
+        sealed class TabbedDestination : Destination()
+
+        @Serializable
+        data object Home : TabbedDestination()
+
+        @Serializable
+        data object Albums : TabbedDestination()
+
+        @Serializable
+        data object Artists : TabbedDestination()
+
+        @Serializable
+        data object Playlists : TabbedDestination()
+
+        @Serializable
+        data object Me : TabbedDestination()
+    }
 
     @Serializable
-    data object Albums : Destination()
+    data object PlayingQueue : Destination()
 
     @Serializable
-    data object Artists : Destination()
+    data object Browse : Destination()
 
     @Serializable
-    data object Playlists : Destination()
+    data class Search(val query: String) : Destination()
+
+    @Serializable
+    data object Downloads : Destination()
+
+    @Serializable
+    data object Notifications : Destination()
+
+    @Serializable
+    data object History : Destination()
+
+    @Serializable
+    data object Favorites : Destination()
+
+    @Serializable
+    data object Wrapped : Destination()
+
+    @Serializable
+    data object PlaylistSurprise : Destination()
+
+    @Serializable
+    data object Settings : Destination()
+
+    @Serializable
+    data object Equalizer : Destination()
 }
