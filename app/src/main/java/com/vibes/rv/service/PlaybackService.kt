e90bd04fe60d7795546539b053b2fb93501a7231
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-12 19:51:14 +0300

    chore: bind ExoPlayer to PlaybackService

--- app/src/main/java/com/vibes/rv/service/PlaybackService.kt
+++ app/src/main/java/com/vibes/rv/service/PlaybackService.kt
@@ -1,10 +1,27 @@
 package com.vibes.rv.service
 
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
 class PlaybackService : MediaSessionService() {
-    private lateinit var mediaSession: MediaSession
+    private var mediaSession: MediaSession? = null
+    private lateinit var exoPlayer: ExoPlayer
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? = mediaSession
+
+    override fun onCreate() {
+        super.onCreate()
+        exoPlayer = ExoPlayer.Builder(this).build()
+        mediaSession = MediaSession.Builder(this, exoPlayer).build()
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+        super.onDestroy()
+    }
 }
