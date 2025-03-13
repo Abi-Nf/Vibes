ff7169a38e5c823b32cdc6c1842d038a0055b8be
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-13 11:35:56 +0300

    chore: add foreign key constraint for playlistId

--- app/src/main/java/com/vibes/rv/data/model/PlaylistItem.kt
+++ app/src/main/java/com/vibes/rv/data/model/PlaylistItem.kt
@@ -1,10 +1,19 @@
 package com.vibes.rv.data.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.vibes.rv.util.now
 
-@Entity
+@Entity(foreignKeys = [
+    ForeignKey(
+        entity = Playlist::class,
+        parentColumns = ["id"],
+        childColumns = ["playlistId"],
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE
+    )
+])
 data class PlaylistItem(
     @PrimaryKey(autoGenerate = true)
     val id: Long? = null,
