33c1891e87dd74f476e277b67e148a13f6c2b772
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-15 00:33:06 +0300

    chore: add some Player function extensions

---
+++ app/src/main/java/com/vibes/rv/util/player/PlayerExtensions.kt
@@ -0,0 +1,42 @@
+package com.vibes.rv.util.player
+
+import androidx.media3.common.Player
+import com.vibes.rv.data.dto.Track
+
+fun Player.playAt(index: Int) {
+    seekTo(index, 0)
+    play()
+}
+
+fun Player.addTrack(track: Track) {
+    addMediaItem(track.toMediaItem())
+}
+
+fun Player.addTracks(tracks: List<Track>) {
+    addMediaItems(tracks.map { it.toMediaItem() })
+}
+
+fun Player.addPlayNext(track: Track) {
+    addMediaItem(currentMediaItemIndex + 1, track.toMediaItem())
+}
+
+fun Player.addAllNext(tracks: List<Track>) {
+    addMediaItems(currentMediaItemIndex + 1, tracks.map { it.toMediaItem() })
+}
+
+fun Player.triggerLoop(): Int {
+    val value = when(repeatMode) {
+        Player.REPEAT_MODE_OFF -> Player.REPEAT_MODE_ONE
+        Player.REPEAT_MODE_ONE -> Player.REPEAT_MODE_ALL
+        Player.REPEAT_MODE_ALL -> Player.REPEAT_MODE_OFF
+        else -> Player.REPEAT_MODE_OFF
+    }
+    repeatMode = value
+    return value
+}
+
+fun Player.toggleShuffle(): Boolean {
+    val value = !shuffleModeEnabled
+    shuffleModeEnabled = value
+    return value
+}
