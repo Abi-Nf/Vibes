f2ef5b2f8bb818e1689dd956753997c816ca7166
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-15 09:21:17 +0300

    active destination watcher

--- app/src/main/java/com/vibes/rv/ui/util/Runtimes.kt
+++ app/src/main/java/com/vibes/rv/ui/util/Runtimes.kt
@@ -2,8 +2,13 @@ package com.vibes.rv.ui.util
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.get
 import com.vibes.rv.data.preference.ThemeMode
 import com.vibes.rv.data.preference.rememberUserThemeMode
 
@@ -20,4 +25,10 @@ fun isUserAppDark(): Boolean {
         ThemeMode.LIGHT -> false
         ThemeMode.DARK -> true
     }
+}
+
+@Composable
+fun <T: Any> NavHostController.isActive(destination: T): State<Boolean> {
+    val entry by currentBackStackEntryAsState()
+    return remember { derivedStateOf { graph[destination] == entry?.destination } }
 }
