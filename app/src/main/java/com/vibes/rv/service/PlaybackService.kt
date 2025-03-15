27db66aff878c53b88be90c54d57baebf858cf0a
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-15 19:07:44 +0300

    feat: open app from media controller in quick settings

--- app/src/main/java/com/vibes/rv/service/PlaybackService.kt
+++ app/src/main/java/com/vibes/rv/service/PlaybackService.kt
@@ -1,8 +1,17 @@
 package com.vibes.rv.service
 
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.annotation.OptIn
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.DefaultMediaNotificationProvider
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import com.vibes.rv.MainActivity
+import com.vibes.rv.R
 
 class PlaybackService : MediaSessionService() {
     private var mediaSession: MediaSession? = null
@@ -10,10 +19,36 @@ class PlaybackService : MediaSessionService() {
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? = mediaSession
 
+    @OptIn(UnstableApi::class)
     override fun onCreate() {
         super.onCreate()
-        exoPlayer = ExoPlayer.Builder(this).build()
-        mediaSession = MediaSession.Builder(this, exoPlayer).build()
+        setMediaNotificationProvider(
+            DefaultMediaNotificationProvider.Builder(this).build().apply {
+                setSmallIcon(R.mipmap.ic_launcher_round)
+            }
+        )
+
+        exoPlayer = ExoPlayer.Builder(this)
+            .setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+                    .setUsage(C.USAGE_MEDIA)
+                    .build(), true
+            )
+            .build()
+
+        val sessionIntent = Intent(this, MainActivity::class.java)
+        val sessionActivityPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            sessionIntent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        mediaSession = MediaSession
+            .Builder(this, exoPlayer)
+            .setSessionActivity(sessionActivityPendingIntent)
+            .build()
     }
 
     override fun onDestroy() {
@@ -23,5 +58,10 @@ class PlaybackService : MediaSessionService() {
             mediaSession = null
         }
         super.onDestroy()
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        stopSelf()
     }
 }
