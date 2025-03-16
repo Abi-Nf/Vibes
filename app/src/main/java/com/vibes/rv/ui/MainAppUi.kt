675224ac866625d38dd2c0c9cf7031a0a6429f56
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-16 12:35:31 +0300

    use custom AppLayout

--- app/src/main/java/com/vibes/rv/ui/MainAppUi.kt
+++ app/src/main/java/com/vibes/rv/ui/MainAppUi.kt
@@ -1,5 +1,7 @@
 package com.vibes.rv.ui
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
@@ -7,16 +9,14 @@ import androidx.compose.animation.scaleOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.vibes.rv.VibesViewModel
-import com.vibes.rv.ui.provider.AppContext
+import com.vibes.rv.ui.component.BottomTabBar
+import com.vibes.rv.ui.layout.vibe_layout.VibeLayout
 import com.vibes.rv.ui.provider.ProvideAppContext
 import com.vibes.rv.ui.screen.detail.album.AlbumsDetails
 import com.vibes.rv.ui.screen.detail.artist.ArtistDetails
@@ -37,60 +37,63 @@ import com.vibes.rv.ui.screen.search.SearchResult
 import com.vibes.rv.ui.screen.setting.AppSettings
 import com.vibes.rv.ui.screen.wrapped.Wrapped
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun MainAppUi(viewModel: VibesViewModel) {
     ProvideAppContext(viewModel) {
-        Box(
-            Modifier
-                .fillMaxSize()
-                .background(AppContext.palette.crust)
-        ) {
-            val controller = rememberNavController()
+        SharedTransitionLayout {
+            VibeLayout {
+                val controller = rememberNavController()
 
-            NavHost(
-                controller,
-                startDestination = Destination.Tabbed,
-                enterTransition = { scaleIn(initialScale = 0.6f) + fadeIn() },
-                exitTransition = { scaleOut() + fadeOut() }
-            ) {
-                composable<Destination.Tabbed> {
-                    val subController = rememberNavController()
+                NavHost(
+                    controller,
+                    startDestination = Destination.Tabbed,
+                    enterTransition = { scaleIn(initialScale = 0.6f) + fadeIn() },
+                    exitTransition = { scaleOut() + fadeOut() }
+                ) {
+                    composable<Destination.Tabbed> {
+                        val subController = rememberNavController()
 
-                    NavHost(
-                        subController,
-                        Destination.Tabbed.Home,
-                        enterTransition = { slideInHorizontally() + fadeIn() },
-                        exitTransition = { slideOutHorizontally() + fadeOut() }
-                    ) {
-                        composable<Destination.Tabbed.Home> { HomeScreen() }
-                        composable<Destination.Tabbed.Artists> { ArtistLibrary() }
-                        composable<Destination.Tabbed.Albums> { AlbumLibrary() }
-                        composable<Destination.Tabbed.Playlists> { PlaylistLibrary() }
-                        composable<Destination.Tabbed.Me> { MySettings() }
+                        DisposableBarContent(60.dp) {
+                            BottomTabBar(subController)
+                        }
+
+                        NavHost(
+                            subController,
+                            Destination.Tabbed.Home,
+                            enterTransition = { slideInHorizontally() + fadeIn() },
+                            exitTransition = { slideOutHorizontally() + fadeOut() }
+                        ) {
+                            composable<Destination.Tabbed.Home> { HomeScreen() }
+                            composable<Destination.Tabbed.Artists> { ArtistLibrary() }
+                            composable<Destination.Tabbed.Albums> { AlbumLibrary() }
+                            composable<Destination.Tabbed.Playlists> { PlaylistLibrary() }
+                            composable<Destination.Tabbed.Me> { MySettings() }
+                        }
                     }
+
+                    composable<Destination.PlayingQueue>(
+                        enterTransition = { slideInVertically() + fadeIn() },
+                        exitTransition = { slideOutHorizontally() + fadeOut() }
+                    ) { PlayingQueue() }
+
+                    composable<Destination.PlaylistSurprise> { PlaylistDetails() }
+                    composable<Destination.AlbumDetails> { AlbumsDetails() }
+                    composable<Destination.ArtistDetails> { ArtistDetails() }
+
+                    composable<Destination.Browse> { BrowseSearch() }
+                    composable<Destination.Search> { SearchResult() }
+
+                    composable<Destination.Downloads> { Downloads() }
+                    composable<Destination.Notifications> { Notifications() }
+
+                    composable<Destination.History> { History() }
+                    composable<Destination.Favorites> { Favorites() }
+                    composable<Destination.Wrapped> { Wrapped() }
+
+                    composable<Destination.Settings> { AppSettings() }
+                    composable<Destination.Equalizer> { Equalizer() }
                 }
-
-                composable<Destination.PlayingQueue>(
-                    enterTransition = { slideInVertically() + fadeIn() },
-                    exitTransition = { slideOutHorizontally() + fadeOut() }
-                ) { PlayingQueue() }
-
-                composable<Destination.PlaylistSurprise> { PlaylistDetails() }
-                composable<Destination.AlbumDetails> { AlbumsDetails() }
-                composable<Destination.ArtistDetails> { ArtistDetails() }
-
-                composable<Destination.Browse> { BrowseSearch() }
-                composable<Destination.Search> { SearchResult() }
-
-                composable<Destination.Downloads> { Downloads() }
-                composable<Destination.Notifications> { Notifications() }
-
-                composable<Destination.History> { History() }
-                composable<Destination.Favorites> { Favorites() }
-                composable<Destination.Wrapped> { Wrapped() }
-
-                composable<Destination.Settings> { AppSettings() }
-                composable<Destination.Equalizer> { Equalizer() }
             }
         }
     }
