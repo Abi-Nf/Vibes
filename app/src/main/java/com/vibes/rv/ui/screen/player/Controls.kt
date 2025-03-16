0ae8878899dd2bbd8f4539dd8c7ca65a242c561a
Abi-Nf <hei.abigail.3@gmail.com>
2025-03-16 12:34:16 +0300

    add player screen

---
+++ app/src/main/java/com/vibes/rv/ui/screen/player/Controls.kt
@@ -0,0 +1,84 @@
+package com.vibes.rv.ui.screen.player
+
+import androidx.annotation.OptIn
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.media3.common.util.UnstableApi
+import com.composables.icons.lucide.Heart
+import com.composables.icons.lucide.Lucide
+import com.vibes.rv.ui.component.Icon
+import com.vibes.rv.ui.component.player.PlayerControl
+import com.vibes.rv.ui.component.player.PlayerPanel
+import com.vibes.rv.ui.component.player.PlayerSlider
+import com.vibes.rv.ui.provider.AppContext
+import com.vibes.rv.ui.state.MusicState
+
+@OptIn(UnstableApi::class)
+@Composable
+internal fun ColumnScope.Controls(musicState: MusicState) {
+    Column(
+        Modifier
+            .weight(1f)
+            .padding(top = 8.dp, bottom = 25.dp)
+            .clip(RectangleShape)
+        ,
+        Arrangement.SpaceBetween
+    ) {
+        val palette = AppContext.palette
+
+        Row(
+            Modifier
+                .padding(horizontal = 26.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            Arrangement.SpaceBetween,
+            Alignment.CenterVertically
+        ) {
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(end = 20.dp)
+            ) {
+                Text(
+                    musicState.currentMedia?.mediaMetadata?.title.toString(),
+                    Modifier.basicMarquee(),
+                    palette.text.principal,
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Text(
+                    musicState.currentMedia?.mediaMetadata?.artist.toString(),
+                    Modifier.basicMarquee(),
+                    palette.lavender,
+                    style = MaterialTheme.typography.titleSmall
+                )
+            }
+
+            Icon(
+                Lucide.Heart,
+                palette.text.principal,
+                Modifier.size(28.dp)
+            )
+        }
+
+        PlayerSlider(
+            musicState.currentTime,
+            musicState.currentMedia?.mediaMetadata?.durationMs ?: 0L
+        )
+        PlayerControl(musicState)
+        PlayerPanel({}, {}, {}, {}, {})
+    }
+}
